d25e822ec33e618a8073791835195e86121b5899
Andrew-E-Anim <andrew.eanim@gmail.com>
2023-10-07 20:25:02 +0000

    Javascript refresher

---
+++ 12_declare_string_variables.js
@@ -0,0 +1,24 @@
+var firstName = "Andrew";
+var middleName = "Emmanuel";
+var lastName = "Anim";
+
+var myStr = "I am a \"double quoted\" string inside \"double quotes\"";
+console.log(myStr);
+
+var myString = '<a href ="http://www.example.com" target="_blank">Link</a>';
+console.log(myString);
+
+var newString = "FirstLine\n\t\\SecondLine\nThirdLine";
+console.log(newString);
+
+var ourStr = "I come first. " + "I come second.";
+
+var myCombinedStr = "This is the start. " + "This is the end.";
+console.log(myCombinedStr);
+
+var ourAugStr = "I come first. "; 
+ourAugStr += "I come second.";
+
+var myAugStr = "This is the start. " 
+myAugStr += "This is the end."; 
+console.log(myAugStr);
