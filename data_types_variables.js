9b314d76b01af16459237af554184ccfb78f326a
Andrew-E-Anim <andrew.eanim@gmail.com>
2023-10-07 10:57:43 +0000

    data types and variables

--- data_types_variables.js
+++ data_types_variables.js
@@ -2,3 +2,11 @@
  * Data types: 
  * undefined, null, boolean, string, symbol, number, and object
  */
+
+var myName = "Andrew E. Anim"
+
+myName = 8
+
+let ourName = "JavaScript Programming"
+
+const pi = 3.14
