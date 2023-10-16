73bb7381cc7d3b2602c014c5535f272516eabd90
Andrew-E-Anim <andrew.eanim@gmail.com>
2023-10-16 23:08:05 +0000

    javasript refresher course

---
+++ 23_object.js
@@ -0,0 +1,33 @@
+var ourDog = {
+    "name": "Camper",
+    "legs": 4,
+    "tails": 1,
+    "friends": ["everything!"]
+};
+
+var ourDog = {
+    "name": "Quincy",
+    "legs": 3,
+    "tails": 2,
+    "friends": []
+};
+
+//accessing objects with the dot notation
+var testObj = {
+    "hat": "ballcap",
+    "shirt": "jersey",
+    "shoes": "cleats"
+};
+
+var hatValue = testObj.hat;
+var shirtValue = testObj.shirt;
+
+//accessing objects with the bracket notation
+var testObj = {
+    "hat": "ballcap",
+    "shirt": "jersey",
+    "shoes": "cleats"
+};
+
+var hatValue = testObj["hat"];
+var shirtValue = testObj["shirt"];
